25f10621336787ec2720b0cf166065be99e9f512
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2024-07-08 21:09:07 -0400

    lazy load About and Detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavMenuComponent } from "./components/nav-menu/nav-menu.component";
+import { DetailComponent } from './components/detail/detail.component';
+import { AboutComponent } from './components/about/about.component';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HomeComponent, 
+    NavMenuComponent,
+    DetailComponent,
+    AboutComponent,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomeComponent, pathMatch:'full'},
+    {
+        path: 'detail', 
+        loadComponent:()=>import('./components/detail/detail.component').then(c=>c.DetailComponent)
+    },
+    {
+        path: 'about', 
+        loadComponent: () => import('./components/about/about.component').then(c=>c.AboutComponent),
+    },
+];
