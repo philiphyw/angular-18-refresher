9460a5114da43b874d8f414bc57c3a0e6551a5dd
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2025-04-30 21:25:15 -0400

    added computed property doubleCount

--- src/app/components/signal-counter/signal-counter.component.ts
+++ src/app/components/signal-counter/signal-counter.component.ts
@@ -1,20 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal-counter',
   standalone: true,
   imports: [],
   templateUrl: './signal-counter.component.html',
-  styleUrl: './signal-counter.component.css'
+  styleUrl: './signal-counter.component.css',
 })
 export class SignalCounterComponent {
-counter = signal<number>(0)
+  counter = signal<number>(0);
+  doubleCounter = computed(() => this.counter() * 2);
 
-onIncrement(){
-  this.counter.update(prev => prev+1)
-}
+  onIncrement() {
+    this.counter.update((prev) => prev + 1);
+  }
 
-onDecrement(){
-  if(this.counter() > 0) this.counter.update(prev => prev-1)
-}
+  onDecrement() {
+    if (this.counter() > 0) this.counter.update((prev) => prev - 1);
+  }
 }
