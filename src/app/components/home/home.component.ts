a5724f98a7cb7fa48adf29dc8cb12e54a64ffd42
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2025-04-30 21:52:23 -0400

    Made counterPrefix property dynamic in homeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HighlightDirective } from '../../directives/highlight.directive';
 import { SignalCounterComponent } from "../signal-counter/signal-counter.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HighlightDirective, SignalCounterComponent],
+  imports: [HighlightDirective, SignalCounterComponent,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  counterPrefix = '';
 }
