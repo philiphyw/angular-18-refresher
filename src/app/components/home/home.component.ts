4d3ecffae74ffade04d64a29d91859d2d3ca347e
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2025-05-01 14:37:19 -0400

    updated output signal with alias

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HighlightDirective } from '../../directives/highlight.directive';
 import { SignalCounterComponent } from "../signal-counter/signal-counter.component";
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HighlightDirective, SignalCounterComponent,FormsModule],
+  imports: [HighlightDirective, SignalCounterComponent,FormsModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   counterPrefix = '';
+  counterActions = signal<string[]>([]);
 
-  onGetCurrentCount(count: number) {
-    console.log(`HomeComponent get current count from SignalComponent: ${count}`);
+  onGetCounterActions(actions: string[]) {
+    this.counterActions.update(() => [...actions]);
   }
 }
--- src/app/components/signal-counter/signal-counter.component.ts
+++ src/app/components/signal-counter/signal-counter.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, effect, input, output, signal } from '@angular/core';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-signal-counter',
@@ -11,16 +12,24 @@ export class SignalCounterComponent {
   counter = signal<number>(0);
   doubleCounter = computed(() => this.counter() * 2);
   printCounter = effect(() =>{console.log(`effect print current counter: ${this.counter()} at ${new Date().toLocaleString()}`)});
+  counterActionLog = signal<string[]>([]);
 
   counterPrefix = input.required<String>();
-  getCurrentCount = output<number>();
+  getCounterActionLog = output<string[]>({
+    alias: 'getActions',
+  });
   onIncrement() {
     this.counter.update((prev) => prev + 1);
-    this.getCurrentCount.emit(this.counter());
+    this.onUpdateCounterActionLog('increment');
   }
   
   onDecrement() {
     if (this.counter() > 0) this.counter.update((prev) => prev - 1);
-    this.getCurrentCount.emit(this.counter());
+    this.onUpdateCounterActionLog('decrement');
+  }
+
+  onUpdateCounterActionLog(action:string) {
+    this.counterActionLog.update((prev) => [...prev, action]);
+    this.getCounterActionLog.emit(this.counterActionLog());
   }
 }
